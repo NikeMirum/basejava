c1b1cc0aed3a3024d8db487d5d5e8935fa7e695d
NikeMirum <75948964+NikeMirum@users.noreply.github.com>
2021-12-16 22:27:10 +0300

    HW1 Update: rebuilding all methods to avoid of null checking

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * Array based storage for Resumes
@@ -9,32 +8,23 @@ public class ArrayStorage {
     int size = 0;
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                storage[i] = null;
-            } else break;
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
         }
         size = 0;
     }
 
     void save(Resume r) {
-        if (storage[storage.length - 1] == null) {
-            for (int i = 0; i < storage.length - 1; i++) {
-                if (storage[i] == null) {
-                    storage[i] = r;
-                    size++;
-                    break;
-                }
-            }
+        if (size < 10000) {
+            storage[size] = r;
+            size++;
         } else System.out.println("В массиве нет свободного места");
     }
 
     Resume get(String uuid) {
-        for (Resume r : storage) {
-            if (r != null) {
-                if (r.uuid.equals(uuid)) {
-                    return r;
-                }
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
@@ -42,17 +32,17 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int delIndex = -1;
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 delIndex = i;
                 break;
             }
         }
         if (delIndex != -1) {
-            for (int i = delIndex; i < storage.length - 1; i++) {
+            for (int i = delIndex; i < size - 1; i++) {
                 storage[i] = storage[i + 1];
             }
-            storage[storage.length - 1] = null;
+            storage[size - 1] = null;
             size--;
         } else System.out.println("Резюме с данным uuid отсутствует");
     }
@@ -61,7 +51,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.stream(storage).filter(Objects::nonNull).toArray(Resume[]::new);
+        return Arrays.stream(storage).limit(size).toArray(Resume[]::new);
     }
 
     int size() {
