4b52770345cfe98a97e33f3f2f33c94c5d42e937
NikeMirum <75948964+NikeMirum@users.noreply.github.com>
2022-03-05 00:04:31 +0300

    HW11 Refactoring: Deadlock Thread start refactoring

--- src/ru/javawebinar/basejava/Deadlock.java
+++ src/ru/javawebinar/basejava/Deadlock.java
@@ -1,41 +1,30 @@
 package ru.javawebinar.basejava;
 
 public class Deadlock {
+    final static Object FIRST_RESOURCE = new Object();
+    final static Object SECOND_RESOURCE = new Object();
 
     public static void main(String[] args) {
-        final Object FIRST_RESOURCE = new Object();
-        final Object SECOND_RESOURCE = new Object();
+        startNewThread(FIRST_RESOURCE, SECOND_RESOURCE);
+        startNewThread(SECOND_RESOURCE, FIRST_RESOURCE);
+    }
 
+    private static void startNewThread(Object o1, Object o2) {
         new Thread(() -> {
-            System.out.println("First Thread is about to take FIRST_RESOURCE");
-            synchronized (FIRST_RESOURCE) {
-                System.out.println("First Thread took FIRST_RESOURCE");
+            System.out.printf("%s is about to take %s%n", Thread.currentThread().getName(), o1);
+            synchronized (o1) {
+                System.out.printf("%s took %s%n", Thread.currentThread().getName(), o1);
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                synchronized (SECOND_RESOURCE) {
-                    System.out.println("First Thread took SECOND_RESOURCE");
+                synchronized (o2) {
+                    System.out.printf("%s took %s%n", Thread.currentThread().getName(), o2);
                 }
             }
-            System.out.println("First Thread passed all synchronized blocks and about to finish its job");
-        }).start();
-
-        new Thread(() -> {
-            System.out.println("Second Thread is about to take SECOND_RESOURCE");
-            synchronized (SECOND_RESOURCE) {
-                System.out.println("Second Thread took FIRST_RESOURCE");
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (FIRST_RESOURCE) {
-                    System.out.println("Second Thread took FIRST_RESOURCE");
-                }
-            }
-            System.out.println("Second Thread passed all synchronized blocks and about to finish its job");
+            System.out.printf("%s passed all synchronized blocks and about to finish its job",
+                    Thread.currentThread().getName());
         }).start();
     }
 }
