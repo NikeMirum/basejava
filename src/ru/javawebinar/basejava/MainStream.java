a0ef3db5031c084334f0f1dd72b6d9410dda327f
NikeMirum <75948964+NikeMirum@users.noreply.github.com>
2022-03-07 21:15:43 +0300

    HW12 Changing: Removing StreamUtil.java and moving all its methods into MainStream

--- src/ru/javawebinar/basejava/MainStream.java
+++ src/ru/javawebinar/basejava/MainStream.java
@@ -1,9 +1,9 @@
 package ru.javawebinar.basejava;
 
 import java.util.Arrays;
-
-import static ru.javawebinar.basejava.util.StreamUtil.minValue;
-import static ru.javawebinar.basejava.util.StreamUtil.oddOrEven;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MainStream {
 
@@ -11,5 +11,18 @@ public class MainStream {
         System.out.println(minValue(new int[]{1, 2, 3, 3, 2, 3}));
         System.out.println(oddOrEven(Arrays.asList(1, 2, 3, 4, 5, 6)));
         System.out.println(oddOrEven(Arrays.asList(1, 2, 3, 4, 5, 7)));
+    }
+
+    public static int minValue(int[] values) {
+        return Arrays.stream(values).
+                distinct().
+                sorted()
+                .reduce(0, (a, b) -> 10 * a + b);
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        final Map<Boolean, List<Integer>> oddOrEvenMap = integers.stream()
+                .collect(Collectors.partitioningBy(i -> i % 2 == 0));
+        return oddOrEvenMap.get(oddOrEvenMap.get(false).size() % 2 != 0);
     }
 }
